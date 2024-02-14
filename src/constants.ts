ec8eb93fdef465058d40ba1efc0ef005061b288a
clakr <clarktolosa@gmail.com>
2024-02-14 20:02:40 +0800

    added links

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,22 @@
 export const links = [
 	{
-		label: "Github",
+		label: "GitHub",
 		href: "https://github.com/clakr/",
+		icon: "icon-[mdi--github]",
+	},
+	{
+		label: "LinkedIn",
+		href: "https://www.linkedin.com/in/clark-tolosa/",
+		icon: "icon-[mdi--linkedin]",
+	},
+	{
+		label: "Resume / Curriculum Vitae",
+		href: "https://ckt.fyi/",
+		icon: "icon-[mdi--resume]",
+	},
+	{
+		label: "frontendmentor.io Projects",
+		href: "https://fm-projects-clakr.vercel.app/",
+		icon: "icon-[simple-icons--frontendmentor]",
 	},
 ];
