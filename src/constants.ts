2a09b76a616361d375e35106091a13c3136067c3
clakr <clarktolosa@gmail.com>
2024-02-17 14:14:39 +0800

    added smaller screens support

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,22 @@
 export const links = [
-	{
-		label: "GitHub",
-		href: "https://github.com/clakr/",
-		icon: "icon-[mdi--github]",
-	},
-	{
-		label: "LinkedIn",
-		href: "https://www.linkedin.com/in/clark-tolosa/",
-		icon: "icon-[mdi--linkedin]",
-	},
-	{
-		label: "Resume / Curriculum Vitae",
-		href: "https://cv.ckt.fyi/",
-		icon: "icon-[mdi--resume]",
-	},
-	{
-		label: "frontendmentor.io Projects",
-		href: "https://fm.ckt.fyi/",
-		icon: "icon-[simple-icons--frontendmentor]",
-	},
+  {
+    label: "GitHub",
+    href: "https://github.com/clakr/",
+    icon: "icon-[mdi--github]",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/clark-tolosa/",
+    icon: "icon-[mdi--linkedin]",
+  },
+  {
+    label: "Resume / Curriculum Vitae",
+    href: "https://cv.ckt.fyi/",
+    icon: "icon-[mdi--resume]",
+  },
+  {
+    label: "frontendmentor.io Projects",
+    href: "https://fm.ckt.fyi/",
+    icon: "icon-[simple-icons--frontendmentor]",
+  },
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,13 @@ const { addDynamicIconSelectors } = require("@iconify/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Inter", ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [addDynamicIconSelectors()],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [addDynamicIconSelectors()],
 };
