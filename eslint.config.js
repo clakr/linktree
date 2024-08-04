81603750bd82e515d34d223d40f81d6f7247ff22
clakr <clarktolosa@gmail.com>
2024-08-04 08:35:51 +0800

    setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import eslintPluginAstro, { rules } from "eslint-plugin-astro";
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  {
+    rules: {},
+  },
+];
